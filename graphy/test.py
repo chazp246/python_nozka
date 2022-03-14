3912b33ee5c84740c6f92a064c16f321634ba7ff
chazp246 <gameboys820@gmail.com>
2022-03-14 12:59:16 +0100

    random bullshit go brrrrrr

--- graphy/test.py
+++ graphy/test.py
@@ -1,13 +1,18 @@
+from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 
-x1 = np.linspace(0, 10, 2000)
-y1 = np.sin(x1)
+t = np.linspace(0, 50e-3, 300000)
+#x1 = np.linspace(0, 10, 2000)
+u = 3.3 * np.sin(2*3.14*50*t)
+u2 = 5 * np.cos(2*3.14*50*t)
 
 
-plt.plot(x1, y1, "g.", label="vzorky")
+plt.plot(t, u, label = "U1")
+plt.plot(t, u2, label = "U2")
+plt.grid()
 plt.legend()
-plt.ylim(-1.5, 1.5)
+#plt.ylim(-1.5, 1.5)
 
 
 plt.show()
